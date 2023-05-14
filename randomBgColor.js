396dde527af7bcada03abb81bde34ee756149af5
James Stewart <jamesrstew@users.noreply.github.com>
2023-05-14 10:31:04 -0700

    color change n stuff

--- randomBgColor.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var colors = ["#D4DED8", "#F8D6DD", "#BDBD93", "#DF8B67", "#F1C35F", "#CFB5A0", "#AD866E"];
-    var randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
-});
